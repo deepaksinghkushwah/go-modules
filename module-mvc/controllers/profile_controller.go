0e449003008ebd76adb5d4ccf7af8af043252fad
Deepak Singh Kuswhah <deepaksinghkushwah@gmail.com>
2020-04-04 10:53:01 +0530

    updates

--- module-mvc/controllers/profile_controller.go
+++ module-mvc/controllers/profile_controller.go
@@ -12,11 +12,14 @@ func Me(c *gin.Context) {
 	session := sessions.Default(c)
 	flash := session.Flashes()
 	session.Save()
+	currentPath := c.Request.URL.Path
+
 	c.HTML(http.StatusOK, "profile.html", gin.H{
-		"Title":      "My Status",
-		"Content":    nil,
-		"IsLoggedIn": session.Get(loggedInKey),
-		"flashes":    flash,
+		"Title":       "My Status",
+		"Content":     nil,
+		"IsLoggedIn":  session.Get(loggedInKey),
+		"flashes":     flash,
+		"currentPath": currentPath,
 	})
 
 }
