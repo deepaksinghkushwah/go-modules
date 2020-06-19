bcb57aa590b974b27504462b019638224ab82323
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2020-06-19 12:47:49 +0530

    updates

--- fyne-test1/main.go
+++ fyne-test1/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fyne.io/fyne"
 	"fyne.io/fyne/app"
+
 	//"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 )
@@ -12,12 +14,22 @@ func main() {
 
 	tabs := widget.NewTabContainer(
 		widget.NewTabItem("Tab 1", widget.NewLabel("Hello")),
-		widget.NewTabItem("Tab 2", widget.NewLabel("World!")))
+		widget.NewTabItem("Tab 2", widget.NewButton("World!", func() {
+			go showAnother(myApp)
+		})))
 
 	//widget.NewTabItemWithIcon("Home", theme.HomeIcon(), widget.NewLabel("Home tab"))
 
 	tabs.SetTabLocation(widget.TabLocationLeading)
 
 	myWindow.SetContent(tabs)
+
 	myWindow.ShowAndRun()
+}
+
+func showAnother(a fyne.App) {
+	win := a.NewWindow("My Another Window")
+	win.SetContent(widget.NewLabel("This is second window"))
+	win.Resize(fyne.NewSize(800, 600))
+	win.Show()
 }
