007a1a846bc8ce6e6c0c7030393e0203756867e3
Deepak Singh Kushwah <deepaksinghkushwah@gmail.com>
2020-06-19 10:28:10 +0530

    updates

--- vc-importer/main.go
+++ vc-importer/main.go
@@ -5,35 +5,35 @@ import (
 	"log"
 	"strconv"
 	"strings"
-	
+
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
 
 func main() {
 	f := excelize.NewFile()
-	f.SetColWidth("Sheet1", "A","H", 20)
-	
+	f.SetColWidth("Sheet1", "A", "H", 20)
 
 	f.SetCellValue("Sheet1", "A1", "Srno")
 	f.SetCellValue("Sheet1", "B1", "LcoID")
 	f.SetCellValue("Sheet1", "C1", "Vcno")
 	f.SetCellValue("Sheet1", "D1", "Recharge_Amount")
 
+	// read number.txt file
 	dat, err := ioutil.ReadFile("./number.txt")
 	checkError(err)
 
 	split := strings.Split(string(dat), "\n")
-	counts := 2	
+	counts := 2
 	for _, item := range split {
 		if len(item) > 0 {
 			nextsplit := strings.Split(item, "Pindi: ")
 			if len(nextsplit) > 1 {
-				mainNumberSplit := strings.Split(nextsplit[1], "+")				
+				mainNumberSplit := strings.Split(nextsplit[1], "+")
 				number, err := strconv.Atoi(mainNumberSplit[0])
 				checkError(err)
-				amount, err := strconv.Atoi(strings.Split(mainNumberSplit[1],"\r")[0]) 
+				amount, err := strconv.Atoi(strings.Split(mainNumberSplit[1], "\r")[0])
 				checkError(err)
-				
+
 				//fmt.Printf("%d of type %T", amount, amount)
 				//fmt.Println("")
 
@@ -52,7 +52,7 @@ func main() {
 	//fmt.Scanln("Press enter to exit")
 }
 
-func checkError(err error){
+func checkError(err error) {
 	if err != nil {
 		log.Fatalln(err)
 	}
